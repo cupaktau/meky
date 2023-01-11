125934bd115b1498615c0e2548bf022a2492bc59
cupaktau <119626857+cupaktau@users.noreply.github.com>
2023-01-11 20:27:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001880703997"))
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "botuna")
 
-# ADMIN
-5832986790
+# ADMIN 5832986790
 # Protect Content
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
